ca49f4f94e27104f4f52948a2ef22f92ea767630
Piotr Groblewski <groblewskip@gmail.com>
2023-09-10 14:48:50 +0200

    map added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 
 function App() {
@@ -17,6 +18,7 @@ useEffect(()=>{
     //console.log(resp)
     setRawData(resp.data)
     setYourIP(resp.data.ip)
+    console.log(rawData)
   })
   .catch((err)=>{
     console.log(err)
@@ -31,7 +33,23 @@ useEffect(()=>{
         <img src="a-little-creepy.jpg" alt="creepy meme" />
         <h3>But here's your IP address :</h3>
         {(!yourIP)? <p>Loading...</p>:<p>{yourIP}</p>}
+      <div>
+        <h3>And the approximate location: </h3>
+      <MapContainer center={[52.51, 13.38]} zoom={5} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[52.51, 13.38]}>
+    <Popup>
+      Are you here?
+    </Popup>
+  </Marker>
+</MapContainer>
       </div>
+      
+      </div>
+
     </>
   )
 }
